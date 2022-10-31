0bbf0d7f41f8bdfad93c99e58cf781754542cded
Tobias van Dijk <tobias@1815.nl>
2022-10-31 23:12:42 +0100

    style: fix WPCS violations and spacing

--- src/Admin.php
+++ src/Admin.php
@@ -17,33 +17,43 @@ use AchttienVijftien\Plugin\Republish\Admin\SetDate;
  */
 class Admin {
 
-    /**
-     * Admin constructor.
-     */
-    public function __construct() {
-        add_action( 'admin_enqueue_scripts', [ $this, 'enqueue_scripts' ] );
-        $this->init();
-    }
+	/**
+	 * Admin constructor.
+	 */
+	public function __construct() {
+		add_action( 'admin_enqueue_scripts', [ $this, 'enqueue_scripts' ] );
+		$this->init();
+	}
 
-    /**
-     * Initialize admin functions.
-     */
-    public function enqueue_scripts( $hook ): void {
+	/**
+	 * Initialize admin functions.
+	 */
+	public function enqueue_scripts( $hook ): void {
 
-        // bail eary if not on post.php
-        if ( 'post.php' !== $hook ) {
-            return;
-        }
+		// bail early if not on post.php.
+		if ( 'post.php' !== $hook ) {
+			return;
+		}
 
-        wp_enqueue_script( 'wp-republish-js', plugin_dir_url( __FILE__ ) . '../assets/js/index.js', array(), '0.1.0' );
-        wp_enqueue_style( 'wp-republish-css', plugin_dir_url( __FILE__ ) . '../assets/css/style.css', array(), '0.1.0' );
-    }
+		wp_enqueue_script(
+			'wp-republish-js',
+			plugin_dir_url( __FILE__ ) . '../assets/js/index.js',
+			[],
+			'0.1.0'
+		);
+		wp_enqueue_style(
+			'wp-republish-css',
+			plugin_dir_url( __FILE__ ) . '../assets/css/style.css',
+			[],
+			'0.1.0'
+		);
+	}
 
-    /**
-     * Initialize admin functions.
-     */
-    public function init(): void {
-        new Settings();
-        new SetDate();
-    }
+	/**
+	 * Initialize admin functions.
+	 */
+	public function init(): void {
+		new Settings();
+		new SetDate();
+	}
 }
